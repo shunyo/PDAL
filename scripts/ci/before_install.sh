ec7b53bd3a51a2d908db40cf6d032c4cbe1ea5f0
Howard Butler <hobu.inc@gmail.com>
2014-04-08 14:49:10 -0500

    try again on boost 1.54

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -6,7 +6,7 @@ sudo apt-get update -y
 sudo apt-get install software-properties-common -y
 sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:ubuntugis/ppa -y
-sudo add-apt-repository ppa:boost-latest/ppa -y
+sudo add-apt-repository ppa:mapnik/boost-backports-1-54 -y
 sudo apt-get update -qq
 sudo apt-get install \
     cmake \
